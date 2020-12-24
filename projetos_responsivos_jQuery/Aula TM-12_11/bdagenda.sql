3834863250360d4141c9dc052b2c3f645b1d5e20
LuizJarduli <luizjarduli@gmail.com>
2020-12-24 17:05:36 -0300

    Projetos Incompletos Removidos

--- projetos_responsivos_jQuery/Aula TM-12_11/bdagenda.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE Bdagenda;
-use Bdagenda;
-
-CREATE TABLE agenda(
-idagenda int primary key auto_increment,
-nome varchar(100) not null,
-email varchar(60) not null unique,
-telefone char(11) not null
-)ENGINE=InnoDB;
